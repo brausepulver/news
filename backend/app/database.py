22177296416590e11dc7896c42eb837111d8c1bf
brausepulver <brausepulver@cock.li>
2024-06-17 19:24:21 +0200

    Initialize database

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,87 @@
+import os
 from databases import Database
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+EMBEDDINGS_SIZE = os.environ.get('EMBEDDINGS_SIZE', 1024)
 database = Database(DATABASE_URL)
+
+async def create_tables(database: Database):
+    await database.execute(f"""
+        CREATE TABLE IF NOT EXISTS "user" (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(255) UNIQUE,
+            username VARCHAR(255) UNIQUE,
+            password_hash VARCHAR(255),
+            google_id VARCHAR(255) UNIQUE,
+            google_token VARCHAR(2048),
+            subscription_status VARCHAR(50) DEFAULT 'inactive',
+            subscription_id VARCHAR(255),
+            preference_text TEXT,
+            preference_embedding vector({EMBEDDINGS_SIZE})
+        );
+    """)
+
+    await database.execute(f"""
+        CREATE TABLE IF NOT EXISTS "sources"(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) UNIQUE,
+            url TEXT
+        );
+    """)
+
+    await database.execute(f"""
+        CREATE TABLE IF NOT EXISTS "articles" (
+            id SERIAL PRIMARY KEY,
+            url TEXT,
+            title TEXT,
+            date TIMESTAMP,
+            summary TEXT,
+            content TEXT,
+            title_embedding vector({EMBEDDINGS_SIZE}),
+            source_id INTEGER REFERENCES sources(id) ON DELETE SET NULL
+        );
+    """)
+
+    await database.execute("""
+        CREATE TABLE IF NOT EXISTS "reports" (
+            user_id INTEGER REFERENCES "user"(id) ON DELETE CASCADE,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (user_id, created_at)
+        );
+    """)
+
+    await database.execute("""
+        CREATE TABLE IF NOT EXISTS "report_sections" (
+            id SERIAL PRIMARY KEY,
+            content TEXT,
+            article_id INTEGER REFERENCES articles(id) ON DELETE SET NULL
+        );
+    """)
+
+    await database.execute("""
+        CREATE TABLE IF NOT EXISTS "report_sections" (
+            id SERIAL PRIMARY KEY,
+            content TEXT,
+            article_id INTEGER REFERENCES articles(id) ON DELETE SET NULL
+        );
+    """)
+
+async def seed_database(database: Database):
+    await database.execute("""
+        INSERT INTO "user" (email, username, password_hash, subscription_status, subscription_id)
+        VALUES ('admin@example.com', 'admin', 'admin', 'active', 'test')
+        ON CONFLICT (email) DO NOTHING;
+    """)
+
+async def initialize_database(database: Database):
+    await database.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+    await create_tables(database)
+    await seed_database(database)
+
+async def tables_exist(database: Database) -> bool:
+    required_tables = {'user', 'sources', 'articles', 'reports', 'report_sections'}
+    existing_tables = await database.fetch_all(
+        "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';"
+    )
+    existing_table_names = {table['table_name'] for table in existing_tables}
+    return required_tables.issubset(existing_table_names)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
-from .database import database
-from .routers import articles
+from routers import articles
+from database import database, initialize_database, tables_exist
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
+    if not await tables_exist(database):
+        await initialize_database(database)
 
 @app.on_event("shutdown")
 async def shutdown():
